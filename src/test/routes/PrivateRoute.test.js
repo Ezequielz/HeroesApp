dea2de8846cbbd1155e3dedce6d3deb127ef14a5
Ezequiel <zapata.ed1989@gmail.com>
2021-04-15 11:31:24 -0300

    test en authReducer y PrivateRoute

---
+++ src/test/routes/PrivateRoute.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { mount } from "enzyme"
+import { PrivateRoute } from '../../routers/PrivateRoute'
+import { MemoryRouter } from 'react-router'
+
+describe('Pruebas en <PrivateRoute />', () => {
+
+    const props = {
+        location:{
+            pathname: '/marvel'
+        }
+    }
+
+    Storage.prototype.setItem = jest.fn();
+
+    test('debe de mostrar el componente si está autenticado y guardar localStorage', () => {
+        
+        const wrapper = mount( 
+
+            <MemoryRouter>
+
+                <PrivateRoute 
+                    isAuthenticated={ true }
+                    component= { () => <span> Listo! </span> }
+                    { ...props }
+    
+                />
+
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('span').exists() ).toBe( true )
+        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel')
+
+
+
+    })
+    
+
+    test('debe de blockear el componente si no esta autenticado', () => {
+        
+        const wrapper = mount( 
+
+            <MemoryRouter>
+
+                <PrivateRoute 
+                    isAuthenticated={ false }
+                    component= { () => <span> Listo! </span> }
+                    { ...props }
+    
+                />
+
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('span').exists() ).toBe( false )
+        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel')
+
+    })
+    
+    
+})
